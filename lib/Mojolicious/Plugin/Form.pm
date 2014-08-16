18b65de3a333e29c2c0ba30bafe4c9cbbc374137
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2014-08-16 09:18:34 +0200

    add travis

--- lib/Mojolicious/Plugin/Form.pm
+++ lib/Mojolicious/Plugin/Form.pm
@@ -48,6 +48,10 @@ __END__
 
 Mojolicious::Plugin::Form - abstract forms for Mojolicious and DBIx::Class
 
+=for html
+
+<a href="https://travis-ci.org/wollmers/Mojolicious-Plugin-Form"><img src="https://travis-ci.org/wollmers/Mojolicious-Plugin-Form.png" alt="Mojolicious-Plugin-Form"></a>
+<a href='https://coveralls.io/r/wollmers/Mojolicious-Plugin-Form?branch=master'><img src='https://coveralls.io/repos/wollmers/Mojolicious-Plugin-Form/badge.png?branch=master' alt='Coverage Status' /></a>
 
 =head1 SYNOPSIS
 
